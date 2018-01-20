51b5c0aefe54aab95ccfa1182c5835a0ff652598
Robert Bossy <Robert.Bossy@inra.fr>
2018-01-20 19:27:56 +0100

    code refactoring

--- js/declatis/main.js
+++ js/declatis/main.js
@@ -1,47 +1,3 @@
-var app = new App({
-	WordRejected: function(word, reason) { console.warn('Rejected because ' + reason + ': ' + word.cleanString); },
-	GenerationFinished: function(words) {
-		words.sort(Word.SCORE_COMPARATOR).reverse();
-		var tab = $('#generated-words');
-		tab.empty();
-		var cols = 4;
-		for (var i = 0; i < words.length; i += cols) {
-			var row = $('<tr></tr>');
-			for (var c = 0; c < cols; ++c) {
-				row.append(this._wordAsCell(words, i + c));
-			}
-			tab.append(row);
-		}
-	},
-	_wordAsCell: function(words, i) {
-		var result = $('<td></td>');
-		if (i < words.length) {
-			var w = words[i];
-			var s = w.cleanString;
-			var cell = $('<div class="btn-group container-fluid btn-group-sm" role="group"></div>').append(
-				$('<button type="button" class="btn btn-light container-fluid" data-toggle="button"></button>').text(s),
-				$('<button type="button" class="btn btn-light icon-info"></button>').popover({
-					title: '<h4>' + w.cleanString + '</h4>',
-					html: true,
-					content: (
-						'<table><tbody>'+
-						'<tr><th class="word-score">Score</th><td>'+w.score.toFixed(4)+'</td></tr>'+
-						'<tr><th class="word-score">Mean probability</th><td>'+w.meanProbability.toFixed(2)+'</td></tr>'+
-						'<tr><th class="word-score">Degradation</th><td>'+w.kDegradationMean.toFixed(2)+'</td></tr>'+
-						'<tr><th class="word-score">Backtracks</th><td>'+w.backtrackCount+'</td></tr>'+
-						'</tbody></table>'
-						),
-					trigger: 'focus',
-					placement: 'top',
-				}),
-			);
-			result.append(cell);
-		}
-		return result;
-	}
-});
-app.wordCount = 40;
-
 class Action {
 	static setK(k) {
 		Action.k = k;
@@ -71,11 +27,57 @@ class Action {
 		app.excludeTrainingWords.push(Dictionary.get(Action.dictionary).matrix);
 		app.generate();
 	}
+
+	static displayWords(words, cols=4) {
+		var tab = $('#generated-words');
+		tab.empty();
+		for (var i = 0; i < words.length; i += cols) {
+			var row = $('<tr></tr>');
+			for (var c = 0; c < cols; ++c) {
+				row.append(Action.wordAsCell(words, i + c));
+			}
+			tab.append(row);
+		}
+	}
+
+	static wordAsCell(words, i) {
+		var result = $('<td></td>');
+		if (i < words.length) {
+			var w = words[i];
+			var s = w.cleanString;
+			var cell = $('<div class="btn-group container-fluid btn-group-sm" role="group"></div>').append(
+				$('<button type="button" class="btn btn-light container-fluid word-string" data-toggle="button"></button>').text(s),
+				$('<button type="button" class="btn btn-light"></button>').popover({
+					title: '<h4>' + w.cleanString + '</h4>',
+					html: true,
+					content: (
+						'<table><tbody>'+
+						'<tr><th class="word-score">Score</th><td>'+w.score.toFixed(4)+'</td></tr>'+
+						'<tr><th class="word-score">Mean probability</th><td>'+w.meanProbability.toFixed(2)+'</td></tr>'+
+						(w.kDegradationMean > 0 ? '<tr class="text-warning"><th class="word-score">Degradation</th><td>'+w.kDegradationMean.toFixed(2)+'</td></tr>' : '')+
+						(w.backtrackCount > 0 ? '<tr class="text-warning"><th class="word-score">Backtracks</th><td>'+w.backtrackCount+'</td></tr>' : '')+
+						'</tbody></table>'
+						),
+					trigger: 'focus',
+					placement: 'top',
+				}).append($('<span></span>').addClass(w.kDegradationMean + w.backtrackCount > 0 ? 'icon-warning text-warning' : 'icon-info text-success')),
+			);
+			result.append(cell);
+		}
+		return result;
+	}
+
 }
 Action.k = 2;
 Action.min = 6;
 Action.max = 10;
 Action.dictionary = 'French proper names';
+
+var app = new App({
+	WordRejected: function(word, reason) { console.warn('Rejected because ' + reason + ': ' + word.cleanString); },
+	GenerationFinished: Action.displayWords,
+});
+app.wordCount = 40;
 
 $(document).ready(function() {
 	var slider = $("#character-count").slider().data('slider');
